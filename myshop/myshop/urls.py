86e43a6945032cfa649a4e07c61231aa0081813f
vimal2406 <patidarvs130@gmail.com>
2021-08-03 12:12:45 +0530

    uploaded images from admin and fetching products list from database

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -3,10 +3,11 @@ from myshop.settings import MEDIA_ROOT, MEDIA_URL
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.templatetags.static import static
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/',include("shop.urls")),
     path('blog/',include("blog.urls"))              
-]  + static(settings.MEDIA_URL, documents_roots= settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,17 +1,25 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nslides = n//4 + ceil((n/4)-(n/4))
+    # params = {'no_of_slides':nslides,'range':range(1,nslides),'product':products}
+    allProds = [[products,range(1,nslides),nslides],[products,range(1,nslides),nslides]]
+    params = {'allProds':allProds}
+    return render(request,'shop/index.html',params)
 
 def about(request):
     return render(request,'shop/about.html')
 
 def contact(request):
     return HttpResponse("We are on contact page")
-
 def tracker(request):
     return HttpResponse("We are on track product page")
 
